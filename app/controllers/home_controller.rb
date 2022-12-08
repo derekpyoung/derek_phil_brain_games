47c089c4e7eb31ce4e3455b4b136b22afcf7e02f
Derek Young <derekpyoung12@gmail.com>
2022-12-08 14:56:03 -0800

    homepage has login/logout/signup features

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+  def index 
+    render template: "home/index"
+  end 
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
 
   post '/login', to: "sessions#create"
   post '/logout', to: "sessions#destroy"
+
+  get '/home', to: "home#index"
  
 end
